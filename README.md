8e4152aa98a53b26cc6e0e835f62e5eb9b0e4e42
Arnau Mora Gras <arnyminerz@proton.me>
2023-05-31 08:46:00 +0200

    Added environment variable for secrets
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

---
+++ README.md
@@ -0,0 +1,4 @@
+# Filà Magenta - Backend
+## Environment Variables
+### `PROPERTIES_FILE`
+If defined, specifies the location of the secrets properties file.
--- src/secrets.mjs
+++ src/secrets.mjs
@@ -6,15 +6,43 @@ import {__dirname} from "./utils.mjs";
 const propertiesFile = path.join(__dirname, '..', 'local.properties');
 
 /**
+ * Checks whether the given path exists or not.
+ * @param {string} file The path of the file to check.
+ * @return {Promise<boolean>}
+ */
+function checkFileExists(file) {
+    return fs.access(file, fs.constants.F_OK)
+        .then(() => true)
+        .catch(() => false)
+}
+
+/**
+ * Checks for the file that secrets should be fetched from. Uses the PROPERTIES_FILE environment variable if defined.
+ * @return {Promise<string>}
+ */
+async function getPropertiesFile() {
+    const envPropertiesFile = process.env.PROPERTIES_FILE;
+    if (envPropertiesFile == null || !(await checkFileExists(envPropertiesFile))) {
+        if (!(await checkFileExists(propertiesFile)))
+            throw new Error('There\'s no local.properties file.');
+        return propertiesFile;
+    }
+    return envPropertiesFile;
+}
+
+/**
  * Reads the contents of the secrets file.
  * @return {Promise<Map<string, string>>}
  */
 async function read() {
-    const contents = await fs.readFile(propertiesFile);
+    const contents = await fs.readFile(await getPropertiesFile());
     const lines = contents.toString().split('\n');
     /** @type {Map<string, string>} */
     const result = new Map();
     for (const line of lines) {
+        if (line.startsWith('#')) continue;
+        if (line.trim().length <= 0) continue;
+
         const pair = line.split('=')
         const key = pair[0];
         const value = pair.slice(1).join('=');
